9be08f6d8aaec62479fcc1e85f32e48f23dc9c3d
Victor Allen <vamuigua@gmail.com>
2020-01-16 20:42:29 +0300

    Add ability of ranged enemy to attack player

---
+++ Assets/EnemyBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    private Player playerScript;
+    private Vector2 targetPosition;
+    public int damage;
+    public float speed;
+    void Start()
+    {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        targetPosition = playerScript.transform.position;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, targetPosition) > 0.1f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerScript.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
