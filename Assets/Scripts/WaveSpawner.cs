ba3559249ce3eec47c8566f927879b2109e78c4d
Victor Allen <vamuigua@gmail.com>
2020-01-17 16:27:29 +0300

    Add WaveSpawner script to manage the spawning of enemy waves

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave
+    {
+        public Enemy[] enemies; //all possible enemies that can spawn in a wave
+        public int count; //how many enemies will spawn in a wave
+        public float timeBetweenSpawns;
+    }
+
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+    public float timeBetweenWaves;
+
+    private Wave currentWave; //stores the current wave in the game
+    private int currentWaveIndex = 0;
+    private Transform player;
+    private bool finishedSpawning;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(StartNextWave(currentWaveIndex));
+    }
+
+    void Update()
+    {
+        //check if the current wave has stopped spawning enemies & if there are null enemies in the scene
+        if (finishedSpawning == true && GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            finishedSpawning = false;
+
+            //check if there is are any remaining waves
+            if (currentWaveIndex + 1 < waves.Length)
+            {
+                currentWaveIndex++;
+                StartCoroutine(StartNextWave(currentWaveIndex));
+            }
+            else
+            {
+                Debug.Log("GAME FINISHED!");
+            }
+        }
+    }
+
+    IEnumerator StartNextWave(int index)
+    {
+        yield return new WaitForSeconds(timeBetweenWaves);
+        StartCoroutine(SpawnWave(index));
+    }
+
+    IEnumerator SpawnWave(int index)
+    {
+        currentWave = waves[index]; //store the current wave
+
+        for (int i = 0; i < currentWave.count; i++)
+        {
+            //if player is dead or is not in the scene stop the couroutine
+            if (player == null)
+            {
+                yield break;
+            }
+
+            //spawn a new random enemy at a random spot
+            Enemy randomEnemy = currentWave.enemies[Random.Range(0, currentWave.enemies.Length)];
+            Transform randomSpot = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomSpot.position, randomSpot.rotation);
+
+            //check if the current wave has finished spwaining enemies
+            if (i == currentWave.count - 1)
+            {
+                finishedSpawning = true;
+            }
+            else
+            {
+                finishedSpawning = false;
+            }
+
+            yield return new WaitForSeconds(currentWave.timeBetweenSpawns);
+        }
+    }
+
+}
